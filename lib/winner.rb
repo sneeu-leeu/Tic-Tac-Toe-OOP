43ed6d45e7188e4af7b0c5acf813d137abaaa901
tahiry-dev <tahirycontact@gmail.com>
2020-10-24 01:28:27 +0300

    Update a class variable into constant

--- lib/winner.rb
+++ lib/winner.rb
@@ -1,17 +1,13 @@
 class Winner
   attr_reader :choices
-  # rubocop:todo Style/ClassVars
-  # rubocop:todo Naming/VariableName
-  @@WINNING_POS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
-  # rubocop:enable Naming/VariableName
-  # rubocop:enable Style/ClassVars
+  WINNING_POS = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]].freeze
 
   def initialize(choices)
     @choices = choices
   end
 
   def real_win?
-    @@WINNING_POS.any? do |combination|
+    WINNING_POS.any? do |combination|
       return true if combination.all? { |element| @choices.include?(element) }
     end
     false
