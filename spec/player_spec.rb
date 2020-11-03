e1a6bd20e608d2d57f9fe96a1f6541a67a59067f
Stephan Annandale <sa070197@gmail.com>
2020-11-03 11:57:52 +0200

    Create tests for board & Player

---
+++ spec/player_spec.rb
@@ -0,0 +1,19 @@
+require './lib/player'
+
+RSpec.describe Players do
+  let(:start) { Players.new('Stephan', 'X', []) }
+  describe 'Player' do
+    it 'Should return the name of the player' do
+      expect(start.name).to eql('Stephan')
+    end
+    it 'Should return the symbol of the player' do
+      expect(start.sign).to eql('X')
+    end
+    it 'Should return an empty array' do
+      expect(start.choice).to eql([])
+    end
+    it 'Should not return a filled array' do
+      expect(start.choice).not_to eql([Integer])
+    end
+  end
+end
