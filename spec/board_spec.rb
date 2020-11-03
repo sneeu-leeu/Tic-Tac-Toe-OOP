77288433baf0d69ac8d27ef6001fe107bd14813c
Stephan Annandale <sa070197@gmail.com>
2020-11-03 12:11:20 +0200

    Fix linters

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,9 +1,9 @@
 require './lib/board'
 RSpec.describe Board do
-  let(:board) {Board.new}
-  let(:valid_move) {8}
-  let(:invalid_move) {12}
-  let(:move1) {[8, 'X']}
+  let(:board) { Board.new }
+  let(:valid_move) { 8 }
+  let(:invalid_move) { 12 }
+  let(:move1) { [8, 'X'] }
   describe '#is_valid?' do
     it 'Should check if the selected move is valid' do
       expect(board.is_valid?(valid_move)).to be(true)
--- spec/winner_spec.rb
+++ spec/winner_spec.rb
@@ -1,12 +1,14 @@
 require './lib/winner'
 
 RSpec.describe Winner do
-  let(:win_choices) {Winner.new([1, 2, 3])}
-  let(:loss_choices) {Winner.new([1, 2, 4])}
-  let(:WINNING_POS) {[[1, 2, 3], [4, 5, 6],
-                     [7, 8, 9], [1, 4, 7],
-                     [2, 5, 8], [3, 6, 9],
-                     [1, 5, 9], [3, 5, 7]].freeze }
+  let(:win_choices) { Winner.new([1, 2, 3]) }
+  let(:loss_choices) { Winner.new([1, 2, 4]) }
+  let(:WINNING_POS) do
+    [[1, 2, 3], [4, 5, 6],
+     [7, 8, 9], [1, 4, 7],
+     [2, 5, 8], [3, 6, 9],
+     [1, 5, 9], [3, 5, 7]].freeze
+  end
   describe '#real_win?' do
     it 'Should let the player win if they have the correct combo' do
       expect(win_choices.real_win?).to be(true)
