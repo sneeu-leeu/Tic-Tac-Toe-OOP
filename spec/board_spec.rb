df9bcffc295b8330bfa37e15e27f3e1b9fbc7c3b
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2020-11-03 11:01:11 +0100

    Add Rspec tests to the game

---
+++ spec/board_spec.rb
@@ -0,0 +1,15 @@
+require './lib/board'
+RSpec.describe Board do
+  let(:board) {Board.new}
+  let(:valid_move) {8}
+  let(:invalid_move) {12}
+  let(:move1) {[8, 'X']}
+  describe '#is_valid?' do
+    it 'Should check if the selected move is valid' do
+      expect(board.is_valid?(valid_move)).to be(true)
+    end
+    it 'Should check if the selected move is invalid' do
+      expect(board.is_valid?(invalid_move)).to be(false)
+    end
+  end
+end
